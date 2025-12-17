8b4275a1dab5c37a1012dc0424cfd743c35fc35b
Athaya Naufal Brasta <athayanaufalbrasta@gmail.com>
2025-12-18 00:43:50 +0700

    fix : forgot to delete this.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AppContextProvider } from "./utils/app-context.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppContextProvider>
-			<App />
-		</AppContextProvider>
+		<App />
 	</StrictMode>,
 );
